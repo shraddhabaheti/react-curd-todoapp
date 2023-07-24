bd5e5b9bc22cdee0a39db82a320f221bc529dfc3
mahesh gandharw <shraddhabeheti123@gmail.com>
2023-07-24 11:09:57 +0530

    todo

--- src/curd/Curd.js
+++ src/curd/Curd.js
@@ -1,92 +1,100 @@
 import React, { useState } from 'react'
 
 const Curd = () => {
-    const[inputData,setInputData]=useState('')
-    const [item,setItem]=useState([])
-    const [toggleButton,setToggleButton]=useState(true)
-    const [isEditItem,setEditItem]=useState(null);
-   const addItem=()=>{
-       if(!inputData){
+    const [inputData, setInputData] = useState('')
+    const [item, setItem] = useState([])
+    const [toggleButton, setToggleButton] = useState(true)
+    const [isEditItem, setEditItem] = useState(null);
 
-       }else if(inputData && !toggleButton){
+
+    const addItem = () => {
+
+        if (!toggleButton ) {
+            
+            let newValue = item.map((elem) => {
+              
+                if (elem.id === isEditItem) {
+                 
+                    elem.name = inputData
+                }
+                return elem;
+            })
+            setItem(newValue)
         
-           setItem(
-             item.map((elem)=>{
-                   if(elem.id === isEditItem){
-
-                       return {...elem,name:inputData};
-                      
-                   }
-                   return elem;
-               })
-               
-           )
-           setToggleButton(true)
-           setEditItem(null)
-           setInputData('')
-       }
-       else{
-            const allinputData={id:new Date().getTime().toString(),name:inputData}
-           setItem([...item,allinputData])
-           setInputData('')
-       }
-   }
-   const deleteItem=(index)=>{
-       const updateItem=item.filter((elem)=>{
-         return index !==elem.id
-       });
-       setItem(updateItem)
-
-   }
-   const removeall=()=>{
-       setItem([]);
-   }
-   const editItem=(id)=>{
-        let newEditItem=item.find((elem)=>{
+            setToggleButton(true)
+            // setEditItem(null)
+            setInputData('')
+        } else {
         
-            return  elem.id ==id
+            const allinputData = { id: new Date().getTime().toString(), name: inputData }
+            if(allinputData?.name !== '' )
+            {
+                // !inputData && item.length &&
+                setItem([...item, allinputData])
+             }
+            setInputData('')
+        }
+    }
+
+    const deleteItem = (index) => {
+        const updateItem = item.filter((elem) => {
+            return index !== elem.id
+        });
+        setItem(updateItem)
+    }
+
+    const removeall = () => {
+        setItem([]);
+    }
+
+    const editItem = (id) => {
+        let newEditItem = item.find((elem) => {
+            return elem.id == id
         })
         setToggleButton(false)
-           setInputData(newEditItem.name)
-           setEditItem(id)
-           
-          
-        
-   }
-   return (
-    <>
-      <div>
-          <h1>Todo app design</h1>
-          <div>
-              <input type="text" placeholder='additem' value={inputData} onChange={(e)=>setInputData(e.target.value)}/>
-              {
-                toggleButton ?<button onClick={addItem}>+</button>:<button onClick={addItem}>-</button>
-              }
-              
-              <div>
-                  {
-                      item.map((elem)=>{
-                          return(
-                              
-                            <div key={elem.id}>
-                            <h3>{elem.name}</h3>
-                            <button onClick={()=>deleteItem(elem.id)}>Delete</button>
-                            <button onClick={()=>editItem(elem.id)}>Edit</button>
-                        </div>
+        setInputData(newEditItem.name)
+        setEditItem(id)
+    }
 
-                          )
+    
+    return (
+        <>
+            <div>
+                <h1 className='h1'>Todo app design.....!</h1>
+                <div>
+                    <input type="text" className='input1' name='text' placeholder='additem....' value={inputData} onChange={(e) => setInputData(e.target.value)} />
 
-                      })
-                  }
-                  
-              </div>
-              <div>
-                  <button onClick={removeall}>CheckList</button>
-              </div>
-          </div>
-      </div>
-    </>
-  )
+                    <button className='btn' onClick={addItem}>{toggleButton ? "Add" : "Edit"}</button>
+
+
+
+                    <div>
+                        {
+                             item.map((elem) => {
+                                return (
+
+                                    <div key={elem.id}>
+
+                                        <h3>{elem.name}</h3>
+                                        <button className='btn' onClick={() => deleteItem(elem.id)}>Delete</button>
+                                        <button className='btn' onClick={() => editItem(elem.id)}>Edit</button>
+                                    </div>
+
+                                )
+
+                            })
+                           
+                        }
+
+                    </div>
+                    <br /><br />
+                    <div>
+                        <button className='btn' onClick={removeall}>CheckList</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Curd
